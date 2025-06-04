67ffaa26af7bfebd2d85d471909694bef8262cb3
HermanL0201 <HermanL0201@users.noreply.github.com>
2025-06-04 18:34:54 +0000

    Minimal Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,3 @@
-/** @type {import('jest').Config} */
 module.exports = {
-  testEnvironment: 'node',
-  verbose: true,
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-  moduleFileExtensions: ['js', 'json', 'node']
+  testEnvironment: 'node'
 };
