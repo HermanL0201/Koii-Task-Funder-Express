0ea27b59080185d64c8b9087ffeef9786e773c7c
HermanL0201 <HermanL0201@users.noreply.github.com>
2025-06-04 18:34:08 +0000

    Simplify Jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
+/** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: 'node',
   verbose: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-  moduleFileExtensions: ['js', 'json', 'node'],
-  transform: {}
+  moduleFileExtensions: ['js', 'json', 'node']
 };
