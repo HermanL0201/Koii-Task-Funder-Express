97d6bb4c9e220089439bcdf1f0faa6a07f9f7c2a
HermanL0201 <HermanL0201@users.noreply.github.com>
2025-06-04 18:33:24 +0000

    Update Jest configuration to support CommonJS

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,7 @@ module.exports = {
   verbose: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)']
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['js', 'json', 'node'],
+  transform: {}
 };
