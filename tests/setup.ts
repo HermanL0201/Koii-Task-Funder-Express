4033a6f54bc43d2dd166f66829a76bb6c4e51a2e
HermanL0201 <HermanL0201@users.noreply.github.com>
2025-06-05 17:44:12 +0000

    Simplify test setup without MongoMemoryServer

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,17 +1,11 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 
-let mongoServer: MongoMemoryServer;
-
 beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  const mongoUri = mongoServer.getUri();
-  await mongoose.connect(mongoUri);
+  await mongoose.connect('mongodb://localhost:27017/test_db');
 });
 
 afterAll(async () => {
   await mongoose.disconnect();
-  await mongoServer.stop();
 });
 
 beforeEach(async () => {
